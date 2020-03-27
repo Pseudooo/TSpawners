1d26bb7f322e391dbe925c966d6d53ccaed2bbe9
pseudo <mitchell.wright877@gmail.com>
2020-03-27 01:50:16 +0000

    init proj

---
+++ src/me/pseudo/TSpawners.java
@@ -0,0 +1,15 @@
+package me.pseudo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TSpawners extends JavaPlugin {
+
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
